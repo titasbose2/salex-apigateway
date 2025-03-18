45c670cb0df3f5a01dd80c883af49c42e1fe8cd3
nabajyoti123 <nabajyoti.sahoo1@gmail.com>
2025-03-18 04:22:01 -0700

    removed unnessary changes

--- src/main/java/com/techlmaginia/salex/ApigatewayApplication.java
+++ src/main/java/com/techlmaginia/salex/ApigatewayApplication.java
@@ -3,10 +3,6 @@ package com.techlmaginia.salex;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
